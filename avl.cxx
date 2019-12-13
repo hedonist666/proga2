158a05a8ea20b48e84a28afa1111bce1f41da214
Валентина Карнаухова <little_scamp@yahoo.com>
2019-12-14 01:32:09 +0300

    final

--- avl.cxx
+++ avl.cxx
@@ -9,13 +9,15 @@
 #include <memory>
 
 
+
 template <typename T>
 struct Node {
   using child = std::shared_ptr<Node<T>>;
-  Node(T e) : left {}, right {}, val {e} { }
+  Node(T e, size_t num = 0) : left {}, right {}, val {e}, num {num} { }
   child left;
   child right;
   T val;
+  size_t num; //ne kruto
 };
 
 
@@ -23,16 +25,17 @@ template <typename T>
 struct AVL {
   using node_t = std::shared_ptr<Node<T>>;
 
-  AVL(std::initializer_list<T> lst) : head {} {
+  AVL(std::initializer_list<T> lst) : head {}, size {} {
     for (auto e : lst) {
       insert(e);
+      ++size;
     }
   }
 
   void insert(const T e) {
     for (auto* cur_p = &head;;) {
       if (!*(cur_p)) {
-        *cur_p = std::make_shared<Node<T>>(e);
+        *cur_p = std::make_shared<Node<T>>(e, size);
         return;
       }
       else {
@@ -47,11 +50,18 @@ struct AVL {
   }
 
   bool verify() {
+    cache.clear();
+    cache.resize(size, 0);
     return check(head);
   }
   void show() const {
+    std::cout << "AVL-tree, size: " << size << std::endl;
     print(head);
   }
+  void pretty_show() const {
+        
+  }
+
 private:
   bool check(node_t cur) {
     if (!cur) return true;
@@ -62,7 +72,8 @@ private:
   } 
   size_t height(node_t cur) {
     if (!cur) return 0;
-    return 1 + std::max(height(cur->left), height(cur->right));
+    if (cache[cur->num]) return cache[cur->num];
+    return (cache[cur->num] = 1 + std::max(height(cur->left), height(cur->right)));
   }
   void print(node_t cur) const {
     if (!cur) return;
@@ -78,12 +89,14 @@ private:
   }
 
   node_t head;
+  size_t size;
+  std::vector<size_t> cache;
 };
 
 
 
 
-/*
+
 int main() {
   using namespace std;
   AVL<int> tr {2, 3, 1, 4, 0, 1, 6, 8};
@@ -91,4 +104,4 @@ int main() {
   cout << boolalpha << tr.verify();
   return 0;
 }
-*/
+
