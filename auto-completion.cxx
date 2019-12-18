05cb870d7105b73a9b3678fb130f3c189852f028
Валентина Карнаухова <little_scamp@yahoo.com>
2019-12-18 06:07:12 +0300

    vrode works

--- auto-completion.cxx
+++ auto-completion.cxx
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <utility>
 #include <array>
@@ -11,59 +12,111 @@
 
 template <typename Container>
 struct Node {
-  std::vector<T> v;
-  std::vector<Node<T>> children;
-  template <typename Cont>
-  Node(Cont lst) : v(lst.begin(), lst.end()) { }
+  Container v;
+  std::vector<Node<Container>> children;
+  template <typename InitContainer>
+  Node(InitContainer lst) : v(lst.begin(), lst.end()) { }
+  template <typename Iter>
+  Node(Iter i1, Iter i2) : v(i1, i2) {}
 };
 
 
-template <typename T, typename Container>
+template <typename T, typename Cr>
 struct Completion {
   
-  std::vector<Container> res;
-  std::vector<Node<T>> head;
+  std::vector<Cr> res;
+  std::vector<Node<Cr>> head;
+  T terminator;
 
-  void cunstruct(std::vector<std::pair<Container, size_t>> v) {
+  Completion(std::istream& is, T terminator) : terminator {terminator}, res {}, head {} {
+    Cr str;
+    size_t pop;    
+    std::vector<std::pair<Cr, size_t>> tmp {};
+    while (is >> str >> pop) {
+      tmp.push_back(std::make_pair(str, pop)); 
+    }
+    construct(tmp);
+  }
+
+  void construct(std::vector<std::pair<Cr, size_t>> v) {
     sort(v.begin(), v.end(), [](auto x, auto y) {
-        return x.second > y.second;
+        return x.second < y.second;
         });
     for (auto& e : v) {
-      insert(head, e.fisrt.begin());
+      insert(head, e.first.begin());
     }
   }
 
   template <typename Iter>
-  void insert(std::vector<Node<T>> cur, Iter input) {
+  bool insert(std::vector<Node<Cr>>& cur, Iter input) {
     for (auto& e : cur) {
-      auto _input = input;
-      auto ch = cur.v.begin();
-      for (;*_input == *ch, ch != cur.end(), *_input; ++ch, ++_input);
-      if (ch == cur.v.end()) {
-        
+      if (*e.v.begin() != *input) {
+        continue;
+      }
+      auto e_it = e.v.begin();
+      for (;*input == *e_it && e_it != e.v.end() && *input != terminator; ++e_it, ++input);
+      if (*input == terminator && e_it == e.v.end()) {
+        return true;
+      }
+      if (e_it == e.v.end()) {
+        if (insert(e.children, input)) return true;
+        return false;
+      }
+      else {
+        auto _input = input;
+        while (*_input != terminator) ++_input;
+        Cr tmp(input, _input + 1);
+        Cr tmp1(e_it, e.v.end());
+        e.v.resize(e_it - e.v.begin());
+        e.children.push_back(tmp);
+        e.children.push_back(tmp1);
+        return true;
       }
     }
+    auto _input = input;
+    while (*_input != terminator) ++_input;
+    cur.push_back(Node<Cr>{input, _input + 1});
+    return true;
+  }
+
+  void show_tree() {
+    _show_tree(head);
+  }
+  void _show_tree(std::vector<Node<Cr>> node) {
+      for (auto& e : node) {
+      std::cout << e.v << ' ';
+      _show_tree(e.children);
+      std::cout << std::endl;
+    }
+  }
+
+  template <typename Iter>
+  void show(Iter input) {
+    Cr tmp {};
+    for (auto& e : head) {
+      run(input, e, tmp);
+    }
+    for (auto& e : res) {
+      std::cout << e << std::endl;
+    }
   }
 
 
-
   template <typename Iter>
-  void run(Iter& input, Node<T>& cur, std::vector<T>& tmp) {
-    if (*input) {
-      auto ch     = cur.v.begin();
-      auto _input = input;
-      for (;ch != cur.v.end(), *ch == *_input; ++ch, ++_input);
-      if (ch == cur.v.end()) {
+  void run(Iter input, Node<Cr>& cur, Cr tmp) {
+    if (*input != terminator) {
+      auto cur_it = cur.v.begin();
+      for (;cur_it != cur.v.end() && *cur_it == *input && *input != terminator; ++cur_it, ++input);
+      if (cur_it == cur.v.end()) {
         for (auto& e : cur.v) {
           tmp.push_back(e);
         }
-        if (!*(cur.v.end() - 1)) {
+        if (cur.children.empty()) {
           res.push_back(tmp);
-          return;
         }
         else {
           for (auto& e : cur.children) {
-            run(_input, e, tmp);
+            run(input, e, tmp);
           }
         }
       }
@@ -72,6 +125,10 @@ struct Completion {
       for (auto& e : cur.v) {
         tmp.push_back(e);
       } 
+      if (cur.children.empty()) {
+        res.push_back(tmp);
+        return;
+      }
       for (auto& e : cur.children) {
         run(input, e, tmp);
       }
@@ -84,6 +141,13 @@ struct Completion {
 
 int main() {
   using namespace std;
-
+  ifstream ifs("input");  
+  char term = '1';
+  Completion<char, string> comp (ifs, '1');
+  string input {"w"};
+  input.push_back(term);
+  comp.show(input.begin());
+  cout << "===============" << endl;
+  comp.show_tree();
   return 0;
 }
